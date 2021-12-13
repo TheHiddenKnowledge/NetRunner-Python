ff1d45512253ee7f0df32e133e049b1fd4664b8d
TheHiddenKnowledge <finney538@gmail.com>
2021-12-13 15:06:31 -0700

    Redesigned the gradient evaluation function to include backpropagation

--- netrunner.py
+++ netrunner.py
@@ -1,39 +1,48 @@
 import numpy as np
 import pygame
 
+
 class net:
-    def __init__(self, inputs, outputs, layers, minOut, maxOut, error):
+    def __init__(self, inputs, outputs, layers, minOut, maxOut, squish):
         self.inputs = []
         self.outputs = []
         self.weights = []
         self.biases = []
+        self.layerout = []
+        self.layersquish = []
+        self.gradientw = []
+        self.gradientb = []
         self.generation = 0
         self.maxInput = inputs
         self.maxOutput = outputs
         self.layers = layers
         self.minOut = minOut
         self.maxOut = maxOut
-        self.error = error
+        self.squish = squish
         self.totalNeurons = []
         self.totalNeurons.append(self.maxInput)
         for i in range(len(layers)):
             self.totalNeurons.append(self.layers[i])
         self.totalNeurons.append(self.maxOutput)
-        for i in range(len(self.totalNeurons)-1):
-            tempw = 1*np.random.random_sample((self.totalNeurons[i+1],self.totalNeurons[i]))-.5
+        for i in range(len(self.totalNeurons) - 1):
+            tempw = 1 * np.random.random_sample((self.totalNeurons[i + 1], self.totalNeurons[i])) - .5
             self.weights.append(tempw)
-            tempb = (self.maxOut-self.minOut)*np.random.random_sample((self.totalNeurons[i+1]))+self.minOut
+            tempb = np.random.random_sample((self.totalNeurons[i + 1]))
             self.biases.append(tempb)
+            self.gradientw.append(np.zeros((self.totalNeurons[i + 1], self.totalNeurons[i])))
+            self.gradientb.append(np.zeros((self.totalNeurons[i + 1])))
+
         self.successes = 0
         self.fails = 0
 
     def randomGenes(self):
         self.weights = []
         self.biases = []
-        for i in range(len(self.totalNeurons)-1):
-            tempw = (self.maxOut-self.minOut)*np.random.random_sample((self.totalNeurons[i+1],self.totalNeurons[i]))-self.minOut
+        for i in range(len(self.totalNeurons) - 1):
+            tempw = (self.maxOut - self.minOut) * np.random.random_sample(
+                (self.totalNeurons[i + 1], self.totalNeurons[i])) - self.minOut
             self.weights.append(tempw)
-            tempb = (self.maxOut-self.minOut)*np.random.random_sample((self.totalNeurons[i+1]))-self.minOut
+            tempb = (self.maxOut - self.minOut) * np.random.random_sample((self.totalNeurons[i + 1])) - self.minOut
             self.biases.append(tempb)
 
     def drawnet(self, screen, width, height, offsetx, offsety):
@@ -59,14 +68,16 @@ class net:
                     point1 = points[i][k]
                     point2 = points[i + 1][j]
                     if self.weights[i][j][k] > 0:
-                        pygame.draw.line(screen, (0, round(255*abs(self.weights[i][j][k]/np.amax(abs(self.weights[i])))), 0), point1, point2)
+                        pygame.draw.line(screen, (
+                        0, round(255 * abs(self.weights[i][j][k] / np.amax(abs(self.weights[i])))), 0), point1, point2)
                     if self.weights[i][j][k] < 0:
-                        pygame.draw.line(screen, (round(255*abs(self.weights[i][j][k]/np.amax(abs(self.weights[i])))), 0, 0), point1, point2)
+                        pygame.draw.line(screen, (
+                        round(255 * abs(self.weights[i][j][k] / np.amax(abs(self.weights[i])))), 0, 0), point1, point2)
         font = pygame.font.Font('freesansbold.ttf', 16)
-        stext = font.render('Successes: '+str(self.successes), True, (255, 255, 255), (0, 0, 0))
+        stext = font.render('Successes: ' + str(self.successes), True, (255, 255, 255), (0, 0, 0))
         textRect = stext.get_rect()
-        w,h = stext.get_size()
-        textRect.center = (width+2*offsetx+w/2, 2*offsety)
+        w, h = stext.get_size()
+        textRect.center = (width + 2 * offsetx + w / 2, 2 * offsety)
         screen.blit(stext, textRect)
         ftext = font.render('Fails: ' + str(self.fails), True, (255, 255, 255), (0, 0, 0))
         textRect = ftext.get_rect()
@@ -75,102 +86,68 @@ class net:
         screen.blit(ftext, textRect)
 
     def sigmoid(self, x):
-        return (self.maxOut-self.minOut)*(1/(1+np.exp(-x/25)))+self.minOut
+        return 1 / (1 + np.exp(-x / self.squish))
 
-    def invsigmoid(self, x):
-        if x > 0:
-            return -25*np.log(((x-self.minOut)/(self.maxOut-self.minOut))**-1-1)
-        else:
-            return 25*np.log(((abs(x) - self.minOut) / (self.maxOut - self.minOut)) ** -1 - 1)
+    def sigderiv(self, x):
+        return (np.exp(x/self.squish)/(np.exp(x/self.squish)+1)**2)/self.squish
 
-    def in2out(self, weights, biases, isOut):
-        layerout = np.array(self.inputs)
-        for i in range(len(weights)):
-            layerout = np.matmul(weights[i], layerout)+biases[i]
-        if isOut:
-            self.outputs = layerout
-        else:
-            return layerout
-
-    def saturateoutput(self):
-        for i in range(self.outputs.shape[0]):
-            self.outputs[i] = self.sigmoid(self.outputs[i])
-
-    def loadnet(self):
-        if len(np.load('bestnet.npz', allow_pickle=True).files) != 0:
-            self.weights = np.load('bestnet.npz', allow_pickle=True)['arr_0']
-            self.biases = np.load('bestnet.npz', allow_pickle=True)['arr_1']
-
-    def savenet(self):
-        np.savez('bestnet', self.weights, self.biases, fmt='%s')
-
-    def gradientDescent(self, expected, step):
-        cost1 = np.square(expected-self.outputs)
+    def in2out(self):
+        current = np.array(self.inputs)
+        self.layerout.append(current)
+        self.layersquish.append(current)
         for i in range(len(self.weights)):
-            for j in range(self.weights[i].shape[0]):
-                neg = False
-                db = self.biases
-                db[i][j] += step
-                cost2 = np.square(expected - self.in2out(self.weights, db, False))
-                difference = cost2 - cost1
-                #print("Bias")
-                #print(cost1)
-                #print(cost2)
-                for m in range(difference.shape[0]):
-                    if difference[m]/step > 0:
-                        self.biases[i][j] -= step
-                        neg = True
-                        break
-                if not neg:
-                    self.biases[i][j] += step
-                for k in range(self.weights[i].shape[1]):
-                    neg = False
-                    dw = self.weights
-                    dw[i][j][k] += step
-                    cost2 = np.square(expected - self.in2out(dw, self.biases, False))
-                    difference = cost2 - cost1
-                    #print("Weight")
-                    #print(cost1)
-                    #print(cost2)
-                    for m in range(difference.shape[0]):
-                        if difference[m]/step > 0:
-                            self.weights[i][j][k] -= step
-                            neg = True
-                            break
-                    if not neg:
-                        self.weights[i][j][k] += step
-        #return s
+            current = np.matmul(self.weights[i], current) + self.biases[i]
+            self.layerout.append(current)
+            for j in range(current.shape[0]):
+                current[j] = self.sigmoid(current[j])
+            self.layersquish.append(current)
+        return current
 
-    def trainNet(self, expectlist, inputlist):
-        for i in range(len(expectlist)):
-            self.inputs = inputlist[i]
-            sum = 0
-            step = 10
-            counter = 0
-            counter1 = 0
-            for m in range(expectlist[i].shape[0]):
-                expectlist[i][m] = self.invsigmoid(expectlist[i][m])
-            while sum < expectlist[i].shape[0]:
-                counter += 1
-                counter1 += 1
-                self.in2out(self.weights, self.biases, True)
-                self.gradientDescent(expectlist[i], step)
-                sum = 0
-                for j in range(expectlist[i].shape[0]):
-                    if (expectlist[i][j] - self.outputs[j]) ** 2 < (expectlist[i][j]*self.error)**2:
-                        sum += 1
-                # if counter > 1100:
-                #     counter = 0
-                #     step *= .1
-                # if counter1 > 5000:
-                #     counter1 = 0
-                #     step = np.random.randint(1,100)
-                #print("A")
-                #print(np.square(expectlist[i]-self.outputs))
-                #print("B")
-                #print(np.square(expectlist[i]*self.error))
-                #print(step)
-            print(self.outputs)
-            print("Completed")
+    def adjustoutput(self):
+        return (self.maxOut - self.minOut) * self.outputs + self.minOut
+
+    def loadnet(self, filename):
+        if len(np.load(filename + '.npz', allow_pickle=True).files) != 0:
+            self.weights = np.load(filename + '.npz', allow_pickle=True)['arr_0']
+            self.biases = np.load(filename + '.npz', allow_pickle=True)['arr_1']
+
+    def savenet(self, filename):
+        np.savez(filename, self.weights, self.biases, fmt='%s')
+
+    def getgradient(self, expected):
+        tempg = []
+        for i in range(len(self.weights)):
+            idx = len(self.weights) - i - 1
+            tempgg = []
+            for j in range(self.weights[idx].shape[0]):
+                if i == 0:
+                    self.gradientb[idx][j] = self.sigderiv(self.layerout[idx + 1][j]) * 2 * (
+                            self.layersquish[idx][j] - expected[j])
+                else:
+                    self.gradientb[idx][j] = self.sigderiv(self.layerout[idx + 1][j]) * \
+                                                tempg[i - 1][j]
+                for k in range(self.weights[idx].shape[1]):
+                    if i == 0:
+                        self.gradientw[idx][j][k] = self.layerout[idx][k] * self.sigderiv(self.layerout[idx+1][j]) * 2 * (
+                                    self.layersquish[idx][j] - expected[j])
+                        if j == 0:
+                            tempgg.append(self.weights[idx][j][k] * self.sigderiv(self.layerout[idx+1][j]) * 2 * (
+                                        self.layersquish[idx+1][j] - expected[j]))
+                        else:
+                            tempgg[k] += self.weights[idx][j][k] * self.sigderiv(self.layerout[idx+1][j]) * 2 * (
+                                    self.layersquish[idx+1][j] - expected[j])
+                    else:
+                        self.gradientw[idx][j][k] = self.layerout[idx][k] * self.sigderiv(self.layerout[idx+1][j]) * tempg[i-1][j]
+                        if j == 0:
+                            tempgg.append(self.weights[idx][j][k] * self.sigderiv(self.layerout[idx+1][j]) * tempg[i - 1][j])
+                        else:
+                            tempgg[k] += self.weights[idx][j][k] * self.sigderiv(self.layerout[idx+1][j]) * tempg[i - 1][j]
+            tempg.append(tempgg)
+            #print(tempg)
+        print(self.weights)
+        print(self.gradientw)
+        print(self.biases)
+        print(self.gradientb)
+
 
 
