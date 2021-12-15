ca26bc9ee6dd81ceb218d8ef1cc0e19876e2192f
TheHiddenKnowledge <finney538@gmail.com>
2021-12-14 17:46:46 -0700

    Included reading sets from provided text files Updated the code to match the updated netrunner module

--- testnet.py
+++ testnet.py
@@ -1,19 +1,12 @@
-import numpy as np
 import netrunner
-import pygame
 
-finished = False
-#pygame.init()
-#field = pygame.display.set_mode((600, 400))
-n = netrunner.net(3, 2, [4,3,3], -10, 10, .25)
-inputs = [[-10, 30, 4],[10,30,4],[100,100,8]]
-expected = [np.array([5, -5]),np.array([-5, -5]),np.array([1, 5])]
-done = False
-#n.loadnet()
-n.trainNet(expected, inputs)
-n.inputs = inputs[0]
-n.in2out(n.weights, n.biases, True)
-n.saturateoutput()
-print(n.outputs)
-n.savenet()
-
+n = netrunner.net(2, 2, [4], -10, 10, 5, 1)
+n.loadnet("bestnet")
+inputs = n.parsesets("inputs.txt")
+expected = n.parsesets("expected.txt")
+print(n.weights)
+n.trainnet(inputs, expected)
+print(n.weights)
+n.inputs = [30, 30]
+print(n.in2out())
+n.savenet("bestnet")
