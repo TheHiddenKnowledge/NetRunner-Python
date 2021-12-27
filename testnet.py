b402cbe77ab0e701a41f17d1ef72bf6b53c7c65a
TheHiddenKnowledge <finney538@gmail.com>
2021-12-27 14:13:22 -0700

    Changed the test to recognize boolean RGB values

--- testnet.py
+++ testnet.py
@@ -1,12 +1,60 @@
+import random
+
 import netrunner
 
-n = netrunner.net(2, 2, [4], -10, 10, 5, 1)
-n.loadnet("bestnet")
-inputs = n.parsesets("inputs.txt")
-expected = n.parsesets("expected.txt")
-print(n.weights)
-n.trainnet(inputs, expected)
-print(n.weights)
-n.inputs = [30, 30]
-print(n.in2out())
-n.savenet("bestnet")
+# This example trains the neural net to recognize colors based on boolean RGB values
+# Initializing the net and net constitutes
+n = netrunner.net(3, 8, [6, 8], .01, .9, 2.5)
+inputs = []
+expected = []
+colors = ["Red", "Green", "Blue", "Yellow", "Purple", "Cyan", "White", "Black"]
+error = 1
+i = 0
+# Continues gradient descent until the error is below the threshold
+while error > .001:
+    tempe = []
+    tempi = []
+    i += 1
+    # Generating the sets of inputs and expected values for gradient descent
+    for j in range(5):
+        color = [random.randint(0, 1), random.randint(0, 1), random.randint(0, 1)]
+        tempi.append(color)
+        if color[0] == 1 and color[1] < 1 and color[2] < 1:
+            tempe.append([1, 0, 0, 0, 0, 0, 0, 0])
+        elif color[0] < 1 and color[1] == 1 and color[2] < 1:
+            tempe.append([0, 1, 0, 0, 0, 0, 0, 0])
+        elif color[0] < 1 and color[1] < 1 and color[2] == 1:
+            tempe.append([0, 0, 1, 0, 0, 0, 0, 0])
+        elif color[0] == 1 and color[1] == 1 and color[2] < 1:
+            tempe.append([0, 0, 0, 1, 0, 0, 0, 0])
+        elif color[0] == 1 and color[1] < 1 and color[2] == 1:
+            tempe.append([0, 0, 0, 0, 1, 0, 0, 0])
+        elif color[0] < 1 and color[1] == 1 and color[2] == 1:
+            tempe.append([0, 0, 0, 0, 0, 1, 0, 0])
+        elif color[0] == 1 and color[1] == 1 and color[2] == 1:
+            tempe.append([0, 0, 0, 0, 0, 0, 1, 0])
+        elif color[0] < 1 and color[1] < 1 and color[2] < 1:
+            tempe.append([0, 0, 0, 0, 0, 0, 0, 1])
+    error = n.getset(tempi, tempe)
+    if i % 100 == 0:
+        print("The error after " + str(i) + " iterations is " + str(error))
+print("The error after " + str(i) + " iterations is " + str(error))
+# Inputting to the neural net and testing each input
+n.inputs = [1, 0, 0]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
+n.inputs = [0, 1, 0]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
+n.inputs = [0, 0, 1]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
+n.inputs = [1, 1, 0]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
+n.inputs = [1, 0, 1]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
+n.inputs = [0, 1, 1]
+out = n.in2out().tolist()
+print(colors[out.index(max(out))])
